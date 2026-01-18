23dc897fa72fd41587c0b8ab5a4f685c42363c10
Abhishek Kumar <61340244+Spyabhishek@users.noreply.github.com>
2026-01-18 20:05:37 +0530

    Comment out security annotations in Hello.java
    Comment out PreAuthorize annotation and RequestMapping.

--- src/main/java/com/printkon/pdp/misc/Hello.java
+++ src/main/java/com/printkon/pdp/misc/Hello.java
@@ -1,12 +1,12 @@
 package com.printkon.pdp.misc;
 
-import org.springframework.security.access.prepost.PreAuthorize;
+// import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/verify")
+// @RequestMapping("/api/verify")
 public class Hello {
 	
 	@GetMapping("/greet")
